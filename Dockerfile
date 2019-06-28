a29795179fda6d3e28fede8ee6a0184d75fab3dd
David Torrejon Vazquez <dtorrejon@MacBook-Pro-de-David.local>
2019-06-28 22:23:11 +0200

    v1 impkementacion hdfs

--- Dockerfile
+++ Dockerfile
@@ -16,10 +16,30 @@ RUN yum install epel-release -y \
 && rpm -ivh jdk-12.0.1_linux-x64_bin.rpm \
 && rm /vol/java/jdk-12.0.1_linux-x64_bin.rpm
 
+ENV HADOOP_HOME /opt/hadoop-3.2.0/
+ENV PATH $PATH:$HADOOP_HOME/bin:$HADOOP_HOME/sbin
+ENV HADOOP_CONF_DIR $HADOOP_HOME/etc/hadoop
+ENV JAVA_HOME /usr/java/jdk-12.0.1/
+
+WORKDIR /
+
+RUN mkdir data
+
+WORKDIR /data
+
+RUN mkdir datanode \
+  && mkdir namenode
+
 COPY /vol/xml/core-site.xml /opt/hadoop-3.2.0/etc/hadoop/core-site.xml
 COPY /vol/xml/hdfs-site.xml /opt/hadoop-3.2.0/etc/hadoop/hdfs-site.xml
 COPY /vol/runscript.sh /root/runscript.sh
-COPY /vol/bashrc.txt /root/.bashrc
+#COPY /vol/bashrc.txt /root/.bashrc
+
+WORKDIR /
+
+RUN echo 'Y' | hdfs namenode -format 2>&1 format.output.log
+
+EXPOSE 9000
 
 WORKDIR /root
 
--- vol/runscript.sh
+++ vol/runscript.sh
@@ -3,16 +3,19 @@
 #RUN ssh-keygen -f id_rsa -t rsa -N ''
 #cp /root/.ssh/* /vol/
 
+#write the hostname in hadoop config files
 sed -i s/masternode/`hostname`/g /opt/hadoop-3.2.0/etc/hadoop/core-site.xml
+
 
 #echo "export JAVA_HOME=/usr/java/jdk-12.0.1/" >> etc/hadoop/hadoop-env.sh
 #ssh-keygen -f id_rsa -t rsa -N ''
 
-#echo "does it works?"
-#mkdir input
-#cp /opt/hadoop-3.2.0/etc/hadoop/*.xml input
-#hadoop jar /opt/hadoop-3.2.0/share/hadoop/mapreduce/hadoop-mapreduce-examples-3.2.0.jar grep input output 'dfs[a-z.]+'
-#cat output/*
+echo "does it works?"
+mkdir input
+mkdir output
+cp /opt/hadoop-3.2.0/etc/hadoop/*.xml input
+hadoop jar /opt/hadoop-3.2.0/share/hadoop/mapreduce/hadoop-mapreduce-examples-3.2.0.jar grep input output 'dfs[a-z.]+'
+cat output/*
 
 
 bash
