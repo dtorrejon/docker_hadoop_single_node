57da4ab93d9ffa53a4ded01b6c7cda9e0e164ee3
dtorrejon <dtorrejon@uoc.edu>
2019-07-01 17:13:39 +0200

    StrictHostKeyChecking no

--- vol/runscript.sh
+++ vol/runscript.sh
@@ -7,23 +7,16 @@ sudo ssh-keygen -f /etc/ssh/ssh_host_rsa_key -t rsa -N ''
 sudo ssh-keygen -f /etc/ssh/ssh_host_dsa_key -t dsa -N ''
 sudo ssh-keygen -f /etc/ssh/ssh_host_ecdsa_key -t ecdsa -N ''
 sudo ssh-keygen -f /etc/ssh/ssh_host_ed25519_key -t ed25519 -N ''
-#sudo ssh-keygen -f /root/.ssh/id_rsa -t rsa -N ''
-ssh-keygen -f $HOME/.ssh/id_rsa -t rsa -N ''
+if [ ! -f $HOME/.ssh/id_rsa ]{
+    ssh-keygen -f $HOME/.ssh/id_rsa -t rsa -N ''
+}
 
 cp $HOME/.ssh/id_rsa.pub $HOME/.ssh/authorized_keys
 cp $HOME/.ssh/id_rsa.pub $HOME/.ssh/known_hosts
-#sudo cp /root/.ssh/id_rsa.pub /root/.ssh/authorized_keys
-#sudo cp /root/.ssh/id_rsa.pub /root/.ssh/known_hosts
-#sudo chmod 0600 /root/.ssh/authorized_keys
-#chmod 0600 $HOME/.ssh/authorized_keys
-#sudo chmod 0600 /root/.ssh/known_hosts
-#chmod 0600 $HOME/.ssh/known_hosts
+
 
 ssh-keyscan -H localhost >> $HOME/.ssh/known_hosts
 ssh-keyscan -H `hostname` >> $HOME/.ssh/known_hosts
-#sudo ssh-keyscan -H localhost >> /root/.ssh/known_hosts
-#sudo ssh-keyscan -H `hostname` >> /root/.ssh/known_hosts
-
 
 #echo "Run sshd service..." 
 #sudo /bin/bash -c /usr/sbin/sshd -D
@@ -32,7 +25,8 @@ ssh-keyscan -H `hostname` >> $HOME/.ssh/known_hosts
 #'Y' | hadoop namenode -format
 #sleep 10
 #echo "Starting HDFS..."
-#start-sleep 15
+#start-dfs.sh
+sleep 15
 #jps
 
 bash
